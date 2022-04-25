50b71468f314f2c14d25743a73deb5a5f3fe5d98
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-04-25 08:17:10 -0500

    files

---
+++ src/UI/Badge.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { slide } from 'svelte/transition'
+</script>
+
+<style>
+  span {
+    display: inline-block;
+    margin: 0 0.25rem;
+    padding: 0 .5rem;
+    background-color: #cf0056;
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+    font-size: 0.8rem;
+    border: 1px solid #cf0056;
+    border-radius: 3px;
+  }
+</style>
+
+<span transition:slide><slot></slot></span>
